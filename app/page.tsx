5412eb6724d43f6330e9fc66bb4e7539b088c454
abhilash <abhilashpal128@gmail.com>
2024-02-10 16:30:55 +0530

    https fixe

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [location, setLocation] = useState("");
   const [error, setError] = useState("");
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_kEY}&q=${location}&days=7&aqi=yes&alerts=yes`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_kEY}&q=${location}&days=7&aqi=yes&alerts=yes`;
 
   const handleSearch = async (e: any) => {
     if (e.key === "Enter") {
